9229e4637b6b01f0f07343dccb818471d7cbd249
sukruthcodes <saisukruthreddy770@gmail.com>
2025-12-29 13:46:21 +0530

    Fix formatting of welcome and success messages

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-print("🎮 Welcome to Number Guessing Game!")
+print(" Welcome to Number Guessing Game!")
 print("I'm thinking of a number between 1 to 100.")
 
 secret_number = random.randint(1, 100)
@@ -15,6 +15,6 @@ while True:
     elif guess > secret_number:
         print("Too high! Try again.")
     else:
-        print(f"🔥 Correct! You guessed it in {attempts} attempts!, successfully")
+        print(f" Correct! You guessed it in {attempts} attempts!, successfully")
         break
 
